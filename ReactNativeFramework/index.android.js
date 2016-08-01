c7919ad88927b307f641e440edbd246f39d8071d
JJ <gongjian_001@126.com>
2016-08-02 06:24:33 +0800

    update

--- ReactNativeFramework/index.android.js
+++ ReactNativeFramework/index.android.js
@@ -8,12 +8,40 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  NativeModules
 } from 'react-native';
 
+var obj = NativeModules.AA
+
 class AwesomeProject extends Component {
+  componentWillMount() {
+    /*
+    //开始加载数据
+    fetch('http://apis.baidu.com/showapi_open_bus/weather_showapi/areaid')
+        .then((response) => response.json())
+        .then((responseJson) => {
+          alert(responseJson.errMsg);
+        }).done();
+        */
+  }
+  componentDidMount()
+  {
+    //obj.callBack((msg) => {alert(msg);})
+    //obj.show();
+    //alert('2222222');
+  }
   render() {
     return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this.onPress}>
+        	<Text style={{fontSize:30,color:'#000'}}>2015</Text>
+        </TouchableHighlight>
+      </View>
+
+
+/*
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -24,8 +52,24 @@ class AwesomeProject extends Component {
         <Text style={styles.instructions}>
           Shake or press menu button for dev menu
         </Text>
+        <TouchableHighlight onPress={this.onPress}>
+        	<Text style={{fontSize:30,color:'#000'}}>2015</Text>
+        </TouchableHighlight>
       </View>
+      */
     );
+  }
+
+  onPress()
+  {
+    //alert('123456');
+	//obj.callBack('123', null); // callback 不能为null
+    obj.callBack('123', (text) => {
+      alert(text);
+    });
+    // obj.processString('123', (text) => {
+//       alert(text);
+//     });
   }
 }
 
